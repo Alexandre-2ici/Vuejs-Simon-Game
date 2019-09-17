099f79f521695468037dbb15a367639a57e39a61
sandrine <sandrinezocca@gmail.com>
2019-09-17 15:36:15 +0200

    sélection des carrés

--- index.js
+++ index.js
@@ -12,11 +12,32 @@ const vm = new Vue({
     },
     methods: {
         // générer la sélection d'une couleur aléatoirement
-        addNewElemToSequanse(){
+        addNewElemToSequense(){
             this.sequence.push(this.squareMapping[Math.floor(Math.random() * 4)]);
         },
+        // remettre tous les carrés au gris
+        allGray() {
+            this.hautGauche = false;
+            this.hautDroite = false;
+            this.basGauche = false;
+            this.basDroite = false;  
+        },
         newGame(){
-            this.addNewElemToSequanse();
+            // réinitialise à chaque nouvelle partie
+            this.sequence = [];
+            // génére la sélection d'une couleur aléatoirement
+            this.addNewElemToSequense();
+            // passer à true pour colorer le carré contenu dans la séquence
+            this[this.sequence[0]] = true;
+            // remettre le carré gris au bout de 300 ms
+            setTimeout(() => {
+                vm.allGray();  
+            }, 300); 
+        },
+        // instruction correspond au carré sur lequel l'utilisateur clique
+        selectSquare(instruction) {
+            console.log(instruction);
+            
         }
     }
 })
