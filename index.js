f47bd638f6877b5710fa0afc352b8587579d2f2c
sandrine <sandrinezocca@gmail.com>
2019-09-17 15:10:51 +0200

    mise en place de la séquence aléatoire

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 const vm = new Vue({
-    el: '#app'
+    el: '#app',
+    data: {
+        // tableau qui contiendra la séquence de carrés qui auront été générés aléatoirement à chaque tour
+        sequence: [],
+        hautGauche: false,
+        hautDroite: false,
+        basGauche: false,
+        basDroite: false,
+        // tableau contenant les quatre carrés et permettra de lier un nombre (0, 1, 2 ou 3 ) a un carré en utilisant l'index du tableau 
+        squareMapping: ['hautGauche', 'hautDroite', 'basDroite', 'basGauche'],
+    },
+    methods: {
+        // générer la sélection d'une couleur aléatoirement
+        addNewElemToSequanse(){
+            this.sequence.push(this.squareMapping[Math.floor(Math.random() * 4)]);
+        },
+        newGame(){
+            this.addNewElemToSequanse();
+        }
+    }
 })
